0511c161a95e11fdb07d6d03c8cd6a65e22d6282
federico nicolas polidoro <federico.nicolas.polidoro@gmail.com>
2023-06-28 14:29:39 -0300

    Añadido Codigo para la CPU
    * Se ha añadido:
      - Macros que controlan las flags del procesador
      - Se ha cambiado de unisgned char a u_int8_t varios valores de la cpu
      - se han añadido constantes con direcciones en memoria (start_addr,
      reset_int, etc...)

--- src/hardware.cpp
+++ src/hardware.cpp
@@ -1,2 +1,30 @@
-#include "hardware_structs.hpp"
+#include "hardware.hpp"
+#include <sys/types.h>
+namespace nes {
+    cpu::cpu(){
+        counter = 0;
+        status = 0;
+        acum = 0;
+        sp = nes::START_STACK_POINTER;
+        rx = 0;
+        ry = 0;
+    }
 
+    u_int8_t cpu::mem_read(u_int16_t addr){
+        return mem[addr];
+
+    }
+
+    void cpu::mem_write(u_int16_t addr, u_int8_t data){
+        mem[addr] = data;
+    }
+
+    u_int16_t cpu::mem_read_u16(u_int16_t addr){
+        u_int16_t lo = (u_int16_t) mem_read(addr);
+        u_int16_t hi = (u_int16_t) mem_read(addr+1);
+        hi = hi << 8;
+        hi |= lo;
+        return hi;
+    }
+
+}
--- src/hardware.hpp
+++ src/hardware.hpp
@@ -1,15 +1,51 @@
 #ifndef HARDWARE_STRUCTS_H_
 #define HARDWARE_STRUCTS_H_
 
+#include <sys/types.h>
+
+#define NEGATIVE_BIT 7
+#define OVERFLOW_BIT 6
+#define BREAK_BIT    4
+#define DECIMAL_BIT  3
+#define INT_BIT      2
+#define CERO_BIT     1
+#define CARRY_BIT    0
+
+#define ESTADO_NEGATIVO  (1 << NEGATIVE_BIT)
+#define ESTADO_OVERFLOW  (1 << OVERFLOW_BIT)
+#define ESTADO_BREAK     (1 << BREAK_BIT)
+#define ESTADO_DECIMAL   (1 << DECIMAL_BIT)
+#define ESTADO_INTERRUPT (1 << INT_BIT)
+#define ESTADO_CERO      (1 << CERO_BIT)
+#define ESTADO_CARRY     CARRY_BIT
+
+#define VALOR_BIT(b,i) ((b & (1 << i)) >> i)
+#define SET_BIT(b,i) b |= (1 << i)
+#define CLEAR_BIT(b,i) b &= ~(1 << i)
+#define ASSIGN_BIT(b,i,v) if (v) { SET_BIT(b,i); } else { CLEAR_BIT(b,i); }
+
 namespace nes {
-    struct cpu {
-        short counter;
-        unsigned char status;
-        unsigned char acum;
-        unsigned char sp;
-        unsigned char rx;
-        unsigned char ry;
-        unsigned char mem[500];
+    const u_int16_t MEM_SIZE = 0xffff;
+    const u_int16_t RESET_INT_ADDR = 0xfffc;
+    const u_int16_t ROM_ADDR_START = 0x0600;
+    const u_int8_t START_STACK_POINTER = 0xff;
+
+    class cpu {
+        public:
+            cpu();
+            u_int8_t mem_read(u_int16_t);
+            void mem_write(u_int16_t, u_int8_t);
+            u_int16_t mem_read_u16(u_int16_t)
+        public:
+            u_int16_t counter;
+            u_int8_t status;
+            u_int8_t acum;
+            u_int8_t sp;
+            u_int8_t rx;
+            u_int8_t ry;
+        private:
+            u_int8_t mem[MEM_SIZE];
+
     };
 
     enum addrMode {
